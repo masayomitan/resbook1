8c0eb0c39560fb7e0d187aaf732d6900ecbfc6e9
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-02-19 20:59:20 +0900

    aaa

--- db/migrate/20200218074510_create_restaurants.rb
+++ db/migrate/20200218074510_create_restaurants.rb
@@ -1,15 +1,16 @@
 class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
-      
+      t.string     :restaurant_name,          null: false
       t.integer    :postal_code,              null: false
       t.string     :address,                  null: false
       t.integer    :tel,                      null: false
       t.integer    :average_price,            null: false
       t.string     :genre,                    null: false
       t.string     :horiday
-      t.string     :drink
-
+      t.string     :drink,                    null: false
+      t.text       :describe,                  null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
