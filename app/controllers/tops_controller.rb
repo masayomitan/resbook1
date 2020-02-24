173c351488ca9f15caa2543e68a4d41774b0025f
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-02-24 23:59:02 +0900

    header

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,7 +2,8 @@ class TopsController < ApplicationController
 
   
   def index
-    @restaurant = Restaurant.all
+    @restaurant = Restaurant.all.order("id DESC").first(6)
+    @image = Image.all.order("id DESC")
   end
 
   def new
