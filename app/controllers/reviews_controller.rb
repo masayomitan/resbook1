a06b7a700066854d506a213f6354d2473acd819d
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-02-28 19:05:13 +0900

    c

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,7 @@ class ReviewsController < ApplicationController
     redirect_to   restaurant_path(@review.restaurant), notice: "レビューしました！"
   end
 
+
   private
   def review_params
     params.require(:review).permit(:comment, :star, :restaurant_id).merge(user_id: current_user.id)
--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,2 +1,8 @@
 module ReviewHelper
+  def raty_stars(n, max=5)
+    (
+      image_tag('raty/star-on.png', alt: '') * n +
+      image_tag('raty/star-off.png', alt: '') * (max-n)
+      ).html_safe
+  end
 end
