f1241473cf91370e17e9a724f6d626a4f3dbd43d
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-03-01 12:59:26 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,11 +2,10 @@ class TopsController < ApplicationController
 
   
   def index
-    @review = Review.find_by(id: params[:id])
     @restaurant = Restaurant.all.order("id DESC").first(6)
     @image = Image.all.order("id DESC")
     @review = Review.new
-    @reviews = @restaurant.reviews.order(created_at: :desc)
+    @reviews = @restaurant.reviews
   end
 
   def new
