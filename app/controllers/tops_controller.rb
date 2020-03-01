da76cfad139289586beb43cf7aaa24518337a9f0
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-03-01 15:00:03 +0900

    r

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,8 +4,8 @@ class TopsController < ApplicationController
   def index
     @restaurant = Restaurant.all.order("id DESC").first(6)
     @image = Image.all.order("id DESC")
-    @review = Review.new
-    @reviews = @restaurant.reviews
+    @reviews = Review.all.order("id DESC")
+    
   end
 
   def new
@@ -13,11 +13,5 @@ class TopsController < ApplicationController
 
   def create
   end
-
-  def show
-    @review = Review.new
-    @reviews = @restaurant.reviews.order(created_at: :desc)
-  end
-
 
 end
