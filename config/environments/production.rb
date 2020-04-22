b45f2e919c4ca5446551e1711bd4b1d3b9531605
川崎聖斗 <kawasakimasato@kawasakiseitonoMacBook-Air.local>
2020-04-22 09:44:49 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.server_static_files = true
+  config.force_ssl = true
 
 
   
@@ -40,6 +41,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
